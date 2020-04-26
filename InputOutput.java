7fe900f45af8f550bb70e9c8b4223790c0ff1b48
Muhammad Rizaldy <muhammadrizaldy1805@gmail.com>
2020-04-26 14:48:07 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -2,11 +2,31 @@ import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
-  Scanner scanner = new Scanner(System.in);
+  Scanner scanner =  new Scanner(System.in);
   String nama;
-  System.out.println("Masukkan nama anda : ");
+  int jumlahSks;
+  double IPK;
+
+  System.out.println("Silahkan disi dulu");
+
+  System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
 
- }
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = scanner.nextInt();
+
+  System.out.print("Masukkan IPK sementara :");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.println("Total Nilai = "+jumlah);
+ 
+  System.out.println("Terimkasih");
+
+  System.out.print("Masukkan nama anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+}
 }
